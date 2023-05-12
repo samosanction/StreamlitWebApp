8cfb6fb117495b7b1b554e5a4f8bcbab3f639203
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-05-12 22:01:38 +0100

    1st Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Our First Streamlit Web App')
+st.subheader('Introducing Streamlit in Automate Everything With Python')
+st.write('''
+This is my first web App
+This is a new line
+''')
