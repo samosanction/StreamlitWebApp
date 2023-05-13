8834381429cdb8af0d9451f7c23ccce0968f45ca
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-05-13 08:23:19 +0100

    added a new slider calculation for Celsius to Fahrenheit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 
 data = {
-    'series 1': [1,3,4,5,7],
+    'series 1': [1, 3, 4, 5, 7],
     'series 2': [10, 30, 40, 50, 70]
 }
 
@@ -16,4 +16,7 @@ This is a new line
 ''')
 st.write(df)
 st.line_chart(df)
-st._arrow_area_chart(df)
+st.bar_chart(df)
+
+my_slider = st.slider('Celsius')
+st.write(my_slider, "in Fahrenheit is: ", my_slider * 9/5 + 32)
